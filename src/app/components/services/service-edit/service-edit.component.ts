31c7d206cfd727e127b13c62ff00a3c6554cc21d
Angel Rivilla <angelrivillaarre@gmail.com>
2021-06-13 14:40:40 +0200

    commit

--- src/app/components/services/service-edit/service-edit.component.ts
+++ src/app/components/services/service-edit/service-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,15 +10,34 @@ import { Router } from '@angular/router';
 export class ServiceEditComponent implements OnInit {
 
   value;
+  servicesForm: FormGroup;
 
-
-  constructor(private router: Router) {
+  constructor(private router: Router, private fb: FormBuilder) {
     const navigation = this.router.getCurrentNavigation();
     this.value = navigation?.extras?.state;
-
+    this.servicesForm = new FormGroup({
+      nombre: new FormControl(),
+      localizacion: new FormControl(),
+      precio: new FormControl(),
+      valoracion: new FormControl(),
+    });
    }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  onSave(): void {
+    console.log('Saved', this.servicesForm.value);
+
+  }
+
+  private initForm(): void {
+    this.servicesForm = this.fb.group({
+      nombre: ['', [Validators.required]],
+      localizacion: ['', [Validators.required]],
+      precio: ['', [Validators.required]],
+      valoracion: ['', [Validators.required]],
+    })
+  }
 }
